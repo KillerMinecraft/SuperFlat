a11f42ea1ddcafd62895c905d2aa159352e1b4d9
FTWinston <ftwinston@gmail.com>
2012-12-07 01:31:12 +0000

    Added extra option names, but this is temporary stuff.
    They don't currently have effect, because I don't think its currently possible to set up a custom superflat world using bukkit.
    Regardless, I intend to make this list just the default options, and have it load options based on config.

--- src/com/ftwinston/Killer/SuperFlat/SuperFlat.java
+++ src/com/ftwinston/Killer/SuperFlat/SuperFlat.java
@@ -8,17 +8,21 @@ import com.ftwinston.Killer.WorldConfig;
 
 public class SuperFlat extends com.ftwinston.Killer.WorldOption
 {
-	public static final int classicFlat = 0, tunnelersDream = 1, waterWorld = 2, overworld = 3, snowyKingdom = 4;
+	public static final int classicFlat = 0, tunnelersDream = 1, waterWorld = 2, overworld = 3, snowyKingdom = 4, bottomlessPit = 5, desert = 6, redstoneReady = 7;
 	
 	@Override
 	public Option[] setupOptions()
 	{
+		// hmm, these options should really be in a config list, if/once we can get it to generate based on the preset codes
 		Option[] options = {
 			new Option("Classic Flat", true),
 			new Option("Tunnelers' Dream", false),
 			new Option("Water World", false),
 			new Option("Overworld", false),
-			new Option("Snowy Kingdom", false)
+			new Option("Snowy Kingdom", false),
+			new Option("Bottomless Pit", false),
+			new Option("Desert", false),
+			new Option("Readstone Ready", false)
 		};
 		
 		return options;
@@ -28,7 +32,7 @@ public class SuperFlat extends com.ftwinston.Killer.WorldOption
 	public void toggleOption(int num)
 	{
 		super.toggleOption(num);
-		Option.ensureOnlyOneEnabled(getOptions(), num, classicFlat, tunnelersDream, waterWorld, overworld, snowyKingdom);
+		Option.ensureOnlyOneEnabled(getOptions(), num, classicFlat, tunnelersDream, waterWorld, overworld, snowyKingdom, bottomlessPit, desert, redstoneReady);
 	}
 	
 	@Override
