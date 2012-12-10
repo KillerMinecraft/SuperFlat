5f4585d8ea46df48a7ab96b16ec84ac948f118d0
Andrew <andrew.watkins@cctechnology.com>
2012-12-10 13:51:26 +0000

    #128 now loading presets from the config, and specifying the default set when that config isn't found

--- src/com/ftwinston/Killer/SuperFlat/Plugin.java
+++ src/com/ftwinston/Killer/SuperFlat/Plugin.java
@@ -1,5 +1,11 @@
 package com.ftwinston.Killer.SuperFlat;
 
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.bukkit.configuration.ConfigurationSection;
+
 import com.ftwinston.Killer.Killer;
 import com.ftwinston.Killer.WorldOption;
 import com.ftwinston.Killer.WorldOptionPlugin;
@@ -8,6 +14,7 @@ public class Plugin extends WorldOptionPlugin
 {
 	public void onEnable()
 	{
+		loadOptions();
 		Killer.registerWorldOption(this);
 	}
 	
@@ -15,5 +22,64 @@ public class Plugin extends WorldOptionPlugin
 	public WorldOption createInstance()
 	{
 		return new SuperFlat();
+	}
+	
+	private static Option[] options = null;
+	
+	private void loadOptions()
+	{
+		ConfigurationSection section = getConfig().getConfigurationSection("worlds");
+		Map<String, Object> optionMap = section == null ? null : section.getValues(false);
+		
+		// if we don't have anything specified, add some defaults
+		if ( optionMap == null || optionMap.size() == 0 )
+		{
+			optionMap = new LinkedHashMap<String, Object>();
+			optionMap.put("Classic Flat", "2;7,2x3,2;1;village");
+			optionMap.put("Tunnelers' Dream", "2;7,230x1,5x3,2;3;stronghold,biome_1,decoration,dungeon,mineshaft");
+			optionMap.put("Water World", "2;7,5x1,5x3,5x12,90x9;1;biome_1,village");
+			optionMap.put("Overworld", "2;7,59x1,3x3,2;1;stronghold,biome_1,village,decoration,dungeon,lake,mineshaft,lava_lake");
+			optionMap.put("Snowy Kingdom", "2;7,59x1,3x3,2,78;12;biome_1,village");
+			optionMap.put("Bottomless Pit", "2;2x4,3x3,2;1;biome_1,village");
+			optionMap.put("Desert", "2;7,3x1,52x24,8x12;2;stronghold,biome_1,village,decoration,dungeon,mineshaft");
+			optionMap.put("Readstone Ready", "2;7,3x1,52x24;2");
+			
+			getConfig().createSection("worlds", optionMap);
+			saveConfig();
+		}
+		
+		options = new Option[optionMap.size()];
+		Iterator<Map.Entry<String, Object>> it = optionMap.entrySet().iterator();
+		int num = 0;
+		while ( it.hasNext() )
+		{
+			Map.Entry<String, Object> entry = (Map.Entry<String, Object>)it.next();
+			options[num++] = new Option(entry.getKey(), (String)entry.getValue());
+		}
+	}
+	
+	static int getNumOptions() { return options.length; }
+	
+	static String getOptionName(int num)
+	{
+		return options[num].getName();
+	}
+	
+	static String getOptionGenerator(int num)
+	{
+		return options[num].getGenerator();
+	}
+	
+	private class Option
+	{
+		public Option(String name, String generator)
+		{
+			this.name = name;
+			this.generator = generator;
+		}
+		
+		private String name, generator;
+		String getName() { return name; }
+		String getGenerator() { return generator; }
 	}
 }
--- src/com/ftwinston/Killer/SuperFlat/SuperFlat.java
+++ src/com/ftwinston/Killer/SuperFlat/SuperFlat.java
@@ -8,31 +8,39 @@ import com.ftwinston.Killer.WorldConfig;
 
 public class SuperFlat extends com.ftwinston.Killer.WorldOption
 {
-	public static final int classicFlat = 0/*, tunnelersDream = 1, waterWorld = 2, overworld = 3, snowyKingdom = 4, bottomlessPit = 5, desert = 6, redstoneReady = 7*/;
-	
 	@Override
 	public Option[] setupOptions()
 	{
-		// hmm, these options should really be in a config list, if/once we can get it to generate based on the preset codes
-		Option[] options = {
-			new Option("Classic Flat", true),
-			/*new Option("Tunnelers' Dream", false),
-			new Option("Water World", false),
-			new Option("Overworld", false),
-			new Option("Snowy Kingdom", false),
-			new Option("Bottomless Pit", false),
-			new Option("Desert", false),
-			new Option("Readstone Ready", false)*/
-		};
-		
-		return options;
+		int num = Plugin.getNumOptions();
+		if ( num < 1 )
+		{
+			Option[] options = {
+				new Option("Classic Flat", true)
+			};
+			
+			return options;
+		}
+		else
+		{
+			Option[] options = new Option[num];
+			
+			for ( int i=0; i<num; i++ )
+				options[i] = new Option(Plugin.getOptionName(i), i==0);
+			
+			return options;
+		}
 	}
 	
 	@Override
 	public void toggleOption(int num)
 	{
 		super.toggleOption(num);
-		Option.ensureOnlyOneEnabled(getOptions(), num, classicFlat/*, tunnelersDream, waterWorld, overworld, snowyKingdom, bottomlessPit, desert, redstoneReady*/);
+		
+		int[] allOptions = new int[Plugin.getNumOptions()];
+		for ( int i=0; i<allOptions.length; i++ )
+			allOptions[i] = i;
+		
+		Option.ensureOnlyOneEnabled(getOptions(), num, allOptions);
 	}
 	
 	@Override
@@ -41,21 +49,14 @@ public class SuperFlat extends com.ftwinston.Killer.WorldOption
 		if ( world.getEnvironment() == Environment.NORMAL )
 		{
 			world.setWorldType(WorldType.FLAT);
-			/*
-			if ( getOption(tunnelersDream).isEnabled() )
-				;
-			else if ( getOption(waterWorld).isEnabled() )
-				;
-			else if ( getOption(overworld).isEnabled() )
-				;
-			else if ( getOption(snowyKingdom).isEnabled() )
-				;
-			else if ( getOption(bottomlessPit).isEnabled() )
-				;
-			else if ( getOption(desert).isEnabled() )
-				;
-			else if ( getOption(redstoneReady).isEnabled() )
-				;*/
+			
+			int numOptions = Plugin.getNumOptions();
+			for ( int i=0; i<numOptions; i++ )
+				if ( getOption(i).isEnabled() )
+				{
+					world.setGeneratorSettings(Plugin.getOptionGenerator(i));
+					break;
+				}
 		}
 		
 		createWorld(world, runWhenDone);
